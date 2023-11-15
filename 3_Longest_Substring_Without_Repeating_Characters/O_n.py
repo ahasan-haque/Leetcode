5fc78270c8b5de47ffa8eb97b04bbe24c7c689c5
S M Ahasanul Haque <smahasanulh@adobe.com>
2023-11-15 17:46:36 +0100

    Leetcode_3 Problem: Longest Substring Without Repeating Characters
    This commit adds a O(n) solution, where a set keeps the substring
    characters, new member added from the right, and duplicates got removed
    from the left.

---
+++ 3_Longest_Substring_Without_Repeating_Characters/O_n.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        right = 0
+        chars = set()
+        result = 0
+
+        while right <= len(s) -1:
+            while s[right] in chars:
+                chars.remove(s[left])
+                left += 1
+            chars.add(s[right])
+            result = max(result, right - left + 1)
+            right += 1
+        return result
